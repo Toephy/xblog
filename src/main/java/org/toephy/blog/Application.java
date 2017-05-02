c94b8d1c72b818fc55f6fc806312e92cc5d28a69
toephy <toephy@163.com>
2017-05-02 17:20:26 +0800

    xblog commit code

--- src/main/java/org/toephy/blog/Application.java
+++ src/main/java/org/toephy/blog/Application.java
@@ -1,7 +1,9 @@
 package org.toephy.blog;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
@@ -9,6 +11,7 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
  * Created by Toephy on 2017.3.29 10:25
  */
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 public class Application extends SpringBootServletInitializer {
 
     @Override
